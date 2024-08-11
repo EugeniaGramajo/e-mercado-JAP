5052c293b2c9538f2c1bbf833982464c8ccf1f10
Bryan <bryanribero@hotmail.com>
2024-08-11 20:29:15 -0300

    mas funcionalidad al boton de venta

---
+++ js/prototipo.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function(){
+    document.getElementById('conteiner-v').addEventListener('click', function(){
+        window.location.href = 'sell.html';
+    });
+});
