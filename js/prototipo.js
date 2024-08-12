1892959b2c56aaaa2ee31eee2db1b89f0c25086b
Bryan <bryanribero@hotmail.com>
2024-08-12 06:12:48 -0300

    termine de incorporar estilo y funcionalidad a las categorias

--- js/prototipo.js
+++ js/prototipo.js
@@ -1,5 +1,22 @@
 document.addEventListener('DOMContentLoaded', function(){
+
     document.getElementById('conteiner-v').addEventListener('click', function(){
         window.location.href = 'sell.html';
     });
+
+    document.querySelectorAll('.conteiner-autos').forEach(container => {
+        let overlay = container.querySelector('.overlay');
+        let content = container.querySelector('.content');
+
+        // Maneja el clic en el SVG para abrir y cerrar la descripción
+        overlay.addEventListener('click', (event) => {
+            content.classList.add('show'); // Alterna la visibilidad
+        });
+
+        // Oculta el contenido cuando el mouse salga del contenedor
+        container.addEventListener('mouseleave', () => {
+            content.classList.remove('show');
+        });
+    });
+    
 });
