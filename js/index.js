338607b181088d3ea4f0d1458ccefdfa3dca9981
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-08-24 20:48:22 -0300

    permanencia de usuario

--- js/index.js
+++ js/index.js
@@ -1,14 +1,41 @@
-document.addEventListener("DOMContentLoaded", function(){
-    document.getElementById("autos").addEventListener("click", function() {
-        localStorage.setItem("catID", 101);
-        window.location = "products.html"
-    });
-    document.getElementById("juguetes").addEventListener("click", function() {
-        localStorage.setItem("catID", 102);
-        window.location = "products.html"
-    });
-    document.getElementById("muebles").addEventListener("click", function() {
-        localStorage.setItem("catID", 103);
-        window.location = "products.html"
-    });
+document.addEventListener("DOMContentLoaded", function () {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const navBar = document.getElementById("navbar");
+  const welcome = document.createElement("li");
+  
+  const logOut = ()=>{
+    localStorage.clear()
+    window.location.href = "login.html";
+  }
+
+  if (user?.name) {
+    welcome.innerHTML = `
+    <div class='d-flex flex-row'>
+    <a class="nav-link" href="my-profile.html">Bienvenid@, ${user.name}!</a>
+    <button class="btn btn-danger" id="logOut">Cerrar Sesión</button>
+    <div>
+    `;
+    navBar.appendChild(welcome);
+  } else {
+    window.location.href = "login.html";
+  }
+
+  const buttonLogOut = document.getElementById("logOut")
+  buttonLogOut.addEventListener("click", ()=>{
+    logOut()
+  })
+  
+
+  document.getElementById("autos").addEventListener("click", function () {
+    localStorage.setItem("catID", 101);
+    window.location = "products.html";
+  });
+  document.getElementById("juguetes").addEventListener("click", function () {
+    localStorage.setItem("catID", 102);
+    window.location = "products.html";
+  });
+  document.getElementById("muebles").addEventListener("click", function () {
+    localStorage.setItem("catID", 103);
+    window.location = "products.html";
+  });
 });
