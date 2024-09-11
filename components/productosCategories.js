922dd6b7bafb691801a95e7f65084d3d5dde3678
EugeniaGramajo <gramajoeugenia@gmail.com>
2024-09-11 20:22:53 -0300

    cambios

---
+++ components/productosCategories.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function (){
+    const productCards = `
+            <div class="container-first">
+        <div class="container-categories">
+               <div class="image-container">
+                   <div class="img-shadow">
+            <img src="img/car1.jpg" class="img-product">
+                   </div>
+                     <div class="hover-background">
+                        <img src="img/icons8-carrito-de-compras-100.png">
+                        <button class="cart-button">add to cart</button>
+                     </div>
+               </div>
+            <div>
+                <h3 class="product-name">Nombre del producto</h3>
+                <p class="product-price">US$23</p>
+            </div>
+        </div>
+    </div>
+    `
+
+
+} )
