84812561ea6fc3b24b8b327c8562052ad12b5ea5
Bryan <bryanribero@hotmail.com>
2024-10-29 19:05:32 -0300

    estilo finalizado, faltaria los productos

---
+++ js/cart-page.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const raiz = document.querySelector(".raiz");
+
+    raiz.innerHTML = `
+<div class="container mt-5 mb-5">
+  <div class="row">
+    <div class="col-md-12">
+      <a href="index.html" class="me-2">Inicio</a> / <span class="ms-2">Cart</span>
+    </div>
+  </div>
+</div>
+    `;
+
+
+    const cuerpo = document.querySelector(".cuerpo-principal");
+
+    const carrito = `
+    
+    `;
+
+
+});
