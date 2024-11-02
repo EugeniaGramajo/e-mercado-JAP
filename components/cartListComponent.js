caf52802b5b245767f00786cd73826c36ed4993c
Camilo Picherno <camilopicherno18@gmail.com>
2024-11-02 16:11:30 -0300

    Boton alineado

--- components/cartListComponent.js
+++ components/cartListComponent.js
@@ -3,7 +3,7 @@ const cartListComponent = (product, updateTotals) => {
 
   const componentHTML = `
     <div class="row align-items-center pt-4 pb-4" data-product-id="${product.id}">
-        <div class="col-sm-3 d-flex align-items-center justify-content-center">
+        <div class="col-sm-3 d-flex align-items-center justify-content-center position-relative">
             <img src="${product.image[0]}" alt="${product.name}" class="img-fluid me-2" style="max-width: 60px; height: auto;">
             <button class="btn btn-danger btn-sm eliminar-producto position-absolute bottom-0 start-0" style="background-color: rgba(255, 0, 0, 0.7); border: none; padding: 0.2rem 0.4rem;">x</button>
             <span class="ms-2">${product.name}</span>
